8e4bd367039cfdaa1a5e689034e58c8aa37fc455
andy <andy@andydote.co.uk>
2014-01-18 23:29:55 +0000

    list all config files

--- brancher.rb
+++ brancher.rb
@@ -4,10 +4,24 @@ $config = YAML.load_file('config.yml')
 
 def find_latest_deploy
 
-	deploy_dir = $config["generic"]["deploy_dir"] + '/'
+	deploy_dir = File.join($config["generic"]["deploy_dir"], "")
 
 	latest = Dir.entries(deploy_dir).sort.reverse[0]
 	
 	path = deploy_dir + latest
 
 end
+
+#Dir.entries(find_latest_deploy).each { |e| puts e }
+
+def list_files_features
+
+	deploy = find_latest_deploy + '/*.yml'
+
+	Dir.glob(deploy) do |file|
+		puts file
+	end
+
+end
+
+list_files_features
