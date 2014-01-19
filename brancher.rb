2074fe3da7b17ffce03a5f32a6ec854c784dbc37
andy <andy@andydote.co.uk>
2014-01-19 16:43:43 +0000

    brancher: write output file, filter input dir for folders only

--- brancher.rb
+++ brancher.rb
@@ -7,13 +7,17 @@ def find_latest_deploy
 
   deploy_dir = File.join($config["generic"]["deploy_dir"], "")
 
-  latest = Dir.entries(deploy_dir).sort.reverse[0]
+  latest = Dir.entries(deploy_dir)
+              .select { |e| File.directory?(File.join(deploy_dir, e)) }
+              .sort
+              .reverse
+              .first
 
   path = deploy_dir + latest
 
 end
 
-def combine_config_files(path)
+def combine_feature_files(path)
 
   deploy = File.join(path, '*.yml')
   files = Dir.glob(deploy)
@@ -34,8 +38,64 @@ def combine_config_files(path)
 
   end
 
-  pp combined
+  return combined
 
 end
 
-combine_config_files(find_latest_deploy)
+def build_sql_file(masterfeature)
+
+  db = $config["db"]
+
+  base = db["root"]
+  preRoot = File.join(base, db["pre_dir"])
+  postRoot = File.join(base, db["post_dir"])
+
+  output = StringIO.new
+  output << "Pre Scripts:\n"
+  output << "--------------------------------------------------------------------------------\n"
+
+  masterfeature[:pre].each { |file|
+
+    output << IO.read(File.join(preRoot, file))
+    output << "\n"
+
+  }
+
+  output << "Sprocs Scripts:\n"
+  output << "--------------------------------------------------------------------------------\n"
+
+  masterfeature[:sprocs].each { |file|
+
+    output << IO.read(File.join(base, file))
+    output << "\n"
+
+  }
+
+  output << "Post Scripts:\n"
+  output << "--------------------------------------------------------------------------------\n"
+
+  masterfeature[:post].each { |file|
+
+    output << IO.read(File.join(postRoot, file))
+    output << "\n"
+
+  }
+
+  return output.string
+
+end
+
+def write_to_output(path, contents)
+
+  File.open(path + ".sql", "w") { |file|
+    file.write(contents)
+  }
+
+end
+
+latestDeploy = find_latest_deploy
+masterfeature = combine_feature_files(latestDeploy)
+
+contents = build_sql_file(masterfeature)
+
+write_to_output(latestDeploy, contents)
