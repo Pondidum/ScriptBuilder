c64b609508523214206d498868eb3c2b3fdb6060
andy <andy@andydote.co.uk>
2014-01-19 15:31:32 +0000

    brancher: change to 2space indent because ruby

--- brancher.rb
+++ brancher.rb
@@ -5,41 +5,42 @@ $config = YAML.load_file('config.yml')
 
 def find_latest_deploy
 
-	deploy_dir = File.join($config["generic"]["deploy_dir"], "")
+  deploy_dir = File.join($config["generic"]["deploy_dir"], "")
 
-	latest = Dir.entries(deploy_dir).sort.reverse[0]
+  latest = Dir.entries(deploy_dir).sort.reverse[0]
 
-	path = deploy_dir + latest
+  path = deploy_dir + latest
 
 end
 
 def combine_config_files(files)
 
-	combined = Hash.new
+  combined = Hash.new
 
-	combined[:pre] = Array.new
-	combined[:sprocs] = Array.new
-	combined[:post] = Array.new
+  combined[:pre] = Array.new
+  combined[:sprocs] = Array.new
+  combined[:post] = Array.new
 
-	files.each do |e|
+  files.each do |e|
 
-		feature = YAML.load_file(e).to_hash
+    feature = YAML.load_file(e).to_hash
 
-		combined[:pre] += feature["pre"]
-		combined[:sprocs] += feature["sprocs"]
-		combined[:post] += feature["post"] unless feature["post"] == nil
+    combined[:pre] += feature["pre"]
+    combined[:sprocs] += feature["sprocs"]
+    combined[:post] += feature["post"] unless feature["post"] == nil
 
-	end
+  end
 
-	pp combined
+  pp combined
+
 end
 
 def list_files_features
 
-	deploy = find_latest_deploy + '/*.yml'
-	configs = Dir.glob(deploy)
+  deploy = find_latest_deploy + '/*.yml'
+  configs = Dir.glob(deploy)
 
-	combine_config_files(configs)
+  combine_config_files(configs)
 
 end
 
